7d1f480d58ecef21325560c387662d6436ca1e7e
lpw <lpw@clivia>
2022-05-06 10:05:32 +0800

    dev

---
+++ bin/latest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+dependencies=(
+ intl
+)
+
+for i in `seq 0 $[${#dependencies[*]}-1]`; do
+  flutter pub remove ${dependencies[i]}
+done
+
+for i in `seq 0 $[${#dependencies[*]}-1]`; do
+  flutter pub add ${dependencies[i]}
+done
+
+flutter pub get
