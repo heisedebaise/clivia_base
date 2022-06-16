cac51b29687a203e57812df529838cafece56216
lpw <lpw@clivia>
2022-06-16 15:55:22 +0800

    dev

--- lib/component/tabview.dart
+++ lib/component/tabview.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Tabview extends StatefulWidget {
+  final String? name;
   final int length;
   final int index;
   final bool tabScrollable;
   final List<Widget> tabs;
   final List<Widget> bodies;
+  final GlobalKey<ScaffoldState> _controllerKey = GlobalKey<ScaffoldState>();
+  static final Map<String, GlobalKey<ScaffoldState>> _map = {};
 
-  const Tabview({Key? key, required this.length, this.index = 0, this.tabScrollable = false, required this.tabs, required this.bodies}) : super(key: key);
+  Tabview({Key? key, this.name, required this.length, this.index = 0, this.tabScrollable = false, required this.tabs, required this.bodies}) : super(key: key) {
+    if (name != null) {
+      _map[name!] = _controllerKey;
+    }
+  }
 
   @override
   State<StatefulWidget> createState() => _TabviewState();
+
+  static void setNavigation(String name, int navigation) {
+    GlobalKey<ScaffoldState>? controllerKey = _map[name];
+    if (controllerKey != null) {
+      DefaultTabController.of(controllerKey.currentContext!)?.animateTo(navigation);
+    }
+  }
 }
 
 class _TabviewState extends State<Tabview> {
@@ -19,6 +33,7 @@ class _TabviewState extends State<Tabview> {
         length: widget.length,
         initialIndex: widget.index,
         child: Scaffold(
+          key: widget._controllerKey,
           appBar: AppBar(
             flexibleSpace: SafeArea(
               child: Column(
