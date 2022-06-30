cf047e19cbd6fe85ced5c57b48c9595e62d37995
lpw <lpw@clivia>
2022-06-30 17:31:31 +0800

    dev

--- lib/component/dividers.dart
+++ lib/component/dividers.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Dividers {
-  static Widget line = const Divider(height: 1);
+  static Widget line = _line(1);
+  static Widget line3 = _line(3);
+  static Widget line5 = _line(5);
+
+  static Widget _line(double height) => Divider(height: height, thickness: height);
 }
