05a0733ef61c1a36c5ac6efb9581fd450bbbc098
lpw <lpw@clivia>
2022-07-06 13:45:04 +0800

    dev

--- lib/component/dividers.dart
+++ lib/component/dividers.dart
@@ -5,5 +5,13 @@ class Dividers {
   static Widget line3 = _line(3);
   static Widget line5 = _line(5);
 
+  static Widget text(String text, {double thickness = 1, double space = 8}) => Row(
+        children: [
+          Expanded(child: _line(thickness)),
+          space > 0 ? Padding(padding: EdgeInsets.symmetric(horizontal: space), child: Text(text)) : Text(text),
+          Expanded(child: _line(thickness)),
+        ],
+      );
+
   static Widget _line(double height) => Divider(height: height, thickness: height);
 }
