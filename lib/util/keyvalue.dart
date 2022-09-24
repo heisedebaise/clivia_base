d533bbe5fead3d7c19478351eb80c4d7e77f5eda
lpw <lpw@clivia>
2022-09-24 18:22:26 +0800

    dev

--- lib/util/keyvalue.dart
+++ lib/util/keyvalue.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'http.dart';
 
 class Keyvalue {
@@ -5,5 +7,12 @@ class Keyvalue {
     dynamic data = await Http.service('/keyvalue/object', data: {'key': key});
 
     return Future.value(data == null ? '' : (data[key] ?? ''));
+  }
+
+  static Future<Map<String, dynamic>> map(String key) async {
+    String string = await value(key);
+    if (string.isEmpty) return {};
+
+    return jsonDecode(string) ?? {};
   }
 }
